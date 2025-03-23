ae949c52403863b352ce3be085802094ab2f41be
Josef Froehle <github@josef-froehle.de>
2025-03-23 03:35:43 +0100

    better build

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,8 +1,16 @@
 import defaultSettings from "../config.page.json" with { type: "json" };
+
+const URLS = {
+	production: "https://www.deepnest.net/",
+	devel_cf: "https://devel.deepnest-net.pages.dev/",
+	development: "http://localhost:8080/",
+	// Add other environments as needed
+};
+
 export default {
 	debug: false,
 	title: "deepnest-next",
-	url: process.env.BUILD_MODE !== "production" ? "http://localhost:8080/" : "https://www.deepnest.net/",
+	url: URLS[process.env.BUILD_MODE] || URLS.development,
 	language: defaultSettings.defaultLanguage,
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
