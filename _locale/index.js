67d9b17bf9a25650ebd7e7f0181ac3cf4509282d
Josef Froehle <github@josef-froehle.de>
2025-03-26 10:14:05 +0100

    sponsors

--- _locale/index.js
+++ _locale/index.js
@@ -1,6 +1,8 @@
 import en from './en.json' with { type: "json" };
 import de from './de.json' with { type: "json" };
+import nl from './nl.json' with { type: "json" };
+import ar from './ar.json' with { type: "json" };
 
 export default {
-  en, de
+  en, de, nl:en, ar:en , "zh-Hant": en
 }
